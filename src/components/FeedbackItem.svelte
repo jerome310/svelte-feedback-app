3a7eb9ccdaa66d504521f8a1c9e90f40e849c829
jerome310 <jeromeyoung333@gmail.com>
2023-06-14 23:21:50 -0400

    updating feedbackitem.svelte file

--- src/components/FeedbackItem.svelte
+++ src/components/FeedbackItem.svelte
@@ -1,25 +1,23 @@
 <script>
-    import { createEventDispatcher } from "svelte";
+  import {FeedbackStore} from '../stores'
+  import Card from './Card.svelte'
+  export let item
 
-    import Card from "./Card.svelte";
-    export let item;
-
-    const dispatch = createEventDispatcher();
-
-    const handleDelete = (itemId) => {
-       dispatch('delete-feedback', itemId)
-    }
+  const handleDelete = (itemId) => {
+    FeedbackStore.update((currentFeedback) => {
+      return currentFeedback.filter(item => item.id != itemId)
+    })
+  }
 </script>
 
-
 <Card>
-    <div class="num-display">
-        {item.rating}
-    </div>
-    <button class="close" on:click={() => handleDelete(item.id)}>X</button>
-     <div class="text-display">
-        {item.text}
-     </div>
+  <div class="num-display">
+    {item.rating}
+  </div>
+  <button class="close" on:click={() => handleDelete(item.id)}>X</button>
+  <p class="text-display">
+    {item.text}
+  </p>
 </Card>
 
 <style>
