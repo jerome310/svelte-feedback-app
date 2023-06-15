ea7f3c7b5d72dcd98619e2665f64fcfe0334b253
jerome310 <jeromeyoung333@gmail.com>
2023-06-14 23:32:02 -0400

    updated app.svelte file

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,11 @@
 <script>
-  import FeedbackList from "./components/FeedbackList.svelte";
-
-  let feedback = [
-    {
-      id: 1,
-      rating: 10,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-    },
-    {
-      id: 2,
-      rating: 9,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-    },
-    {
-      id: 3,
-      rating: 8,
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-    },
-  ];
-
-  $: count = feedback.length;
-  $: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length;
-
-  const deleteFeedback = (e) => {
-      const itemId = e.detail;
-
-      feedback = feedback.filter((item) => item.id != itemId)
-  }
+	import FeedbackForm from './components/FeedbackForm.svelte'
+	import FeedbackList from './components/FeedbackList.svelte'
+	import FeedbackStats from './components/FeedbackStats.svelte'
 </script>
 
 <main class="container">
-  <h1>{average}</h1>
-	<FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
+	<FeedbackForm />
+	<FeedbackStats />
+	<FeedbackList />
 </main>
-
-<style>
-</style>
